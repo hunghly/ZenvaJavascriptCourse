f2eda1dcb46abe159c6e28f005eada386361848b
Hung Ly <hunghly@gmail.com>
2020-04-22 14:08:29 -0500

    learned prototype for javascript

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 {
-    console.log("test");
+    /*console.log("test");
 
     let gameCharacter = {
         name: 'Hung',
@@ -33,6 +33,44 @@
     console.log(hung.maxHealth); // undefined due to being a setter
     console.log(hung.health); // it was changed due to us using setter eariler
 
+*/
+
+    function GameCharacter(name, xPos, health) {
+        this.name = name;
+        this.xPos = xPos;
+        this.health = health;
+        this.move = function (x) {
+            this.xPos += x;
+        }
+        this.class = 'Human';
+        this.getTitle = () => {
+            return this.name + " the " + this.class;
+        }
+    }
+
+    let char1 = new GameCharacter(
+        'hung',
+        0,
+        100
+    );
+
+    console.log(char1.name);
+    console.log(char1.getTitle());
+
+    let heal = function(amount) {
+        this.health += amount;
+    }
+
+    GameCharacter.prototype.weapon = 'Dagger';
+    console.log(char1.weapon);
+
+    let char2 = new GameCharacter('joe', 0, 140);
+    GameCharacter.prototype.heal = heal;
+
+    char1.heal(500);
+    console.log(char1.health);
+    char2.heal(1);
+    console.log(char2.health);
 
 
 }
