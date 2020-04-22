bda31bd543b6af5f8762a7d43536ac792802ce22
Hung Ly <hunghly@gmail.com>
2020-04-22 13:51:30 -0500

    learned javascript getter and setters

--- app.js
+++ app.js
@@ -1,3 +1,38 @@
 {
     console.log("test");
+
+    let gameCharacter = {
+        name: 'Hung',
+        class: 'Human',
+        health: 100,
+        get title() {
+            return this.name + ' the ' + this.class;
+        },
+        set maxHealth(h) {
+            this.health = h;
+        },
+        xPos: 0,
+        items: ['knife', 'food'],
+        move: function(x) {
+            this.xPos += x;
+        }
+    }
+    let name = gameCharacter.name;
+    gameCharacter.items = ['Axe', 'Bread']
+    gameCharacter.yPos = 0;
+    gameCharacter.move = function (x,y) {
+        this.xPos += x;
+        this.yPos += y;
+    }
+    console.log(gameCharacter);
+
+    let hung = gameCharacter;
+    console.log(hung.title);
+
+    hung.maxHealth = 150;
+    console.log(hung.maxHealth); // undefined due to being a setter
+    console.log(hung.health); // it was changed due to us using setter eariler
+
+
+
 }
